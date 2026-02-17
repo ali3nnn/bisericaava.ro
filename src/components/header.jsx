2c38e0a6abe16a095c205f8c5b3c611206d6e797
Alex Barbu <alexbarbu@eon.com>
2026-02-17 19:39:51 +0200

    converted images to webp; added time and location

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -60,6 +60,10 @@ export const Header = (props) => {
               </div>
             </div>
           </div>
+          <div className="intro-bottom">
+            <p><i className="fa fa-clock-o"></i> Duminică de la ora 10 la 12</p>
+            <p><a href="https://maps.app.goo.gl/W85vQDMNpvfqsvA99" target="_blank" rel="noopener noreferrer"><i className="fa fa-map-marker"></i> Strada Tineretului 2, Targoviste</a></p>
+          </div>
         </div>
       </div>
     </header>
