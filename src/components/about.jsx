e60a7c4e80dd38bfb4ce9e35fd92d9a077778b60
Alex Barbu <alexbarbu@eon.com>
2026-02-18 09:46:31 +0200

    added lazy loading

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ export const About = (props) => {
         <div className="row">
           <div className="col-xs-12 col-md-6">
             {" "}
-            <img src="img/despre.webp" className="img-responsive" alt="" />{" "}
+            <img src="img/despre.webp" className="img-responsive" alt="" loading="lazy" />{" "}
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
